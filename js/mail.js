f3f40b591c97ab4924c7cbafb84be3fff12dced4
luciobo <luciobo@icloud.com>
2022-11-03 16:13:58 +0100

    carato ciclo conarray, non funzioante

--- js/mail.js
+++ js/mail.js
@@ -1,6 +1,25 @@
 
-
+const userName = prompt("inserisci la tua mail")
+// console.log(userName)
 //                     0                     1                    2                  3                 4             
-const listaMail = ["mumford@gmail.it", "massive@gmail.com", "rino@gmail.com", "nin@gmail.com", "lucio@gmail.com"]
+const listaMail = ["mumford@gmail.it", "massive@gmail.com", "rino@gmail.com", "nin@gmail.com", "lucio@gmail.com"];
 
-console.log(listaMail)
+// console.log(listaMail);
+
+let trovato = false;
+
+
+for (let i = 0; i < listaMail.length; i++) {
+
+    // console.log((listaMail[i]))
+
+    if (listaMail === userName) {
+
+        alert("utente autorizzato")
+
+    } else {
+
+        alert("untente non autorizzato")
+    }
+
+}
