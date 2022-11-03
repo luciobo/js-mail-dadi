81624a37f64254e6c8ad3a90b025b93a88b60a39
luciobo <luciobo@icloud.com>
2022-11-03 15:14:49 +0100

    creato array

---
+++ js/mail.js
@@ -0,0 +1,6 @@
+
+
+//                     0                     1                    2                  3                 4             
+const listaMail = ["mumford@gmail.it", "massive@gmail.com", "rino@gmail.com", "nin@gmail.com", "lucio@gmail.com"]
+
+console.log(listaMail)
