b1b6fee9eeee0b585f98e37f3c4bb9a91b4cd83c
luciobo <luciobo@icloud.com>
2022-11-04 10:49:44 +0100

    correzioni

--- js/dadi.js
+++ js/dadi.js
@@ -6,9 +6,9 @@ const utente2 = Math.floor(Math.random() * 6 + 1)
 console.log(utente2)
 
 if (utente1 < utente2) {
-    console.log("utente1 uno vince")
+    console.log("utente1  vince")
     alert("Pippo Vince")
 } else {
-    console.log("utente2 uno vince")
+    console.log("utente2  vince")
     alert("Pluto Vince")
 }
